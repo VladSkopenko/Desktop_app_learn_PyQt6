5ba3018427d7621c3a1c2fa515009c8e81c64dd2
VladSkopenko <skopirka2k17@gmail.com>
2024-09-27 13:30:07 +0300

    my lean app

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtWidgets import QMainWindow
 
+from src.static.window import q_main_window_style
+
 
 def application():
     app = QApplication(sys.argv)
     window = QMainWindow()
 
-    window.setWindowTitle("Train app")
+    window.setWindowTitle("Learning app")
     window.setGeometry(400, 300, 300, 200)
+    window.setStyleSheet(q_main_window_style)
 
     window.show()
     sys.exit(app.exec())
---
+++ src/static/window.py
@@ -0,0 +1,7 @@
+q_main_window_style = """
+            QMainWindow {
+                background-color: #f0f0f0;
+                border: 2px solid #4CAF50; 
+                border-radius: 10px;
+            }
+        """
